ca70063867b8cae6a285ace4027323207952b0a1
Akrutijain <akkukothari7001@gmail.com>
2026-02-05 20:34:40 +0530

    Project completed

--- frontend/src/pages/AttendancePage.jsx
+++ frontend/src/pages/AttendancePage.jsx
@@ -116,14 +116,13 @@ function AttendancePage() {
             </tr>
           </thead>
           <tbody>
-                {records.map((r) => (
-                    <tr key={`${r.employee_id}-${r.date}`}>
-                    <td>{r.date}</td>
-                    <td>{r.status}</td>
-                    </tr>
-                ))}
+            {records.map((r, index) => (
+              <tr key={index}>
+                <td>{r.date}</td>
+                <td>{r.status}</td>
+              </tr>
+            ))}
           </tbody>
-
         </table>
       )}
     </div>
